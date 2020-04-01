11b705858c32d3adbff66bd91235d064c2728738
RamAddict <pbiancofriber@gmail.com>
2020-04-01 14:58:23 -0300

    Add display help to bot, fix bot reading its own message

--- app.js
+++ app.js
@@ -12,26 +12,38 @@ function clientHandlerReady(client) {
 
 /** @param {import("discord.js").Message} msg */
 function clientHandlerMesssage(msg) {
+    lowerCaseMessage = msg.content.toLowerCase();
     // for now invocation line is b.
-    if (msg.content.startsWith('b.', 0) == true) {
-        if (msg.content.search('quote') != -1) {
+    if (lowerCaseMessage.startsWith('b.', 0) == true) {
+        // b.Help function
+        if (lowerCaseMessage.search('help') != -1) {
+            const embed = new Discord.MessageEmbed()
+            .setTitle('Bolsonaro command list:')
+            .setColor(0xFCAA00)
+            // .setThumbnail('https://i.imgur.com/lCZ1lnx.jpg')
+            .setDescription('Use b. para falar comigo')
+            .addField('Quote', 'Mando uma elocução do passado', true)
+            .addField('Help', 'Mostro essa mensagem', false);
+            msg.channel.send('Brasil acima de tudo, Deus acima de todos.', embed);
+        }
+        else
+        // b.Quote function
+        if (lowerCaseMessage.search('quote') != -1) {
             msg.channel.send(getRandomLine())
         }
     }
-    
-    //   random text replies section EASTEREGGS
-    if (msg.content.search('adriano') != -1) {
-        msg.reply('Adriano é viado eim')
-    }
-    if (msg.content.search('joao') != -1) {
-        msg.reply('joao é corno')
-    }
-    if (msg.content.search('web dev') != -1 || msg.content.search('webedeve') != -1) {
-        msg.reply('Web dev nem gente é')
-    }
-
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+    // only respond to non bot messages
+    if (!msg.author.bot) {
+        //   random text replies section EASTEREGGS
+        if (lowerCaseMessage.search('adriano') != -1) {
+            msg.reply('Adriano é viado eim')
+        }
+        if (lowerCaseMessage.search('joao') != -1) {
+            msg.reply('joao é corno')
+        }
+        if (lowerCaseMessage.search('web dev') != -1 || lowerCaseMessage.search('webedeve') != -1) {
+            msg.reply('Web dev nem gente é')
+        }
     }
 }
 
@@ -39,7 +51,7 @@ function initializeBot() {
     const client = new Discord.Client();
     client.on('ready', () => clientHandlerReady(client));
     client.on('message', clientHandlerMesssage);
-    client.login('Njk0NjYzNjk1MDg2NjQ5MzY2.XoO-FQ.z2CJqdlEBnX-Hy2s6WJahQsG5ng');
+    client.login('Njk0NjYzNjk1MDg2NjQ5MzY2.XoQBUw.Oxk-eWFIp0hTUnXYX59SnjRPbJw');
 }
 
 // Begin Listen
